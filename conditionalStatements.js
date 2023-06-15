0eb37bdb96885ecc8b6ee7880610d17e865456ab
JavaScriptInsights <136630542+JavaScriptInsights@users.noreply.github.com>
2023-06-15 19:50:36 +0530

    Lecture1
    Lecture1

---
+++ conditionalStatements.js
@@ -0,0 +1,41 @@
+//1)if else
+// let x = false;
+// if(x){
+//     console.log('Inside If Statement');
+// }else{
+//     console.log('Inside Else Statement');
+// }
+
+//2else if
+
+// let y = 4;
+// if(y === 1){
+//     console.log('Inside If Statement');
+// }else if(y === 2){
+//     console.log('Inside 1st Else/If Statement');
+// }else if(y === 3){
+//     console.log('Inside 2nd Else/If Statement');
+// }else{
+//     console.log('Inside Else Statement');
+// }
+
+//3 ternary operators
+// let z = 2;
+// z === 1 ? console.log('Value of z is ' + z) : console.log('Value of z is ' + z);
+
+//4 Switch
+
+let fruit = '';
+switch(fruit){
+    case 'Banana':
+    console.log(fruit);
+    break;
+
+    case 'Apple':
+    console.log(fruit);
+    break
+
+    default:
+    console.log('No Fruit');
+    break;
+}
