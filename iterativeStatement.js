5aab7a05ec0e411d2d89a741abbf1f8d04e1ca54
JavaScriptInsights <136630542+JavaScriptInsights@users.noreply.github.com>
2023-06-20 11:28:15 +0530

    Add files via upload
    Lecture2

---
+++ iterativeStatement.js
@@ -0,0 +1,18 @@
+//for loop
+// for(let i=0,j=0;i<5 & j <2 ;i++,j++){
+//     console.log(`Subscribed to JavascriptInsights ${i} and ${j}`);
+// }
+
+//while loop
+// let i = 0
+// while(i<5){
+//     console.log(`Subscribed to JavascriptInsights ${i}`);
+//     i++;
+// }
+
+//do-while loop
+// let i = 5;
+// do{
+//     console.log(`Subscribed to JavascriptInsights ${i}`);
+//     i++;
+// }while(i < 5)
